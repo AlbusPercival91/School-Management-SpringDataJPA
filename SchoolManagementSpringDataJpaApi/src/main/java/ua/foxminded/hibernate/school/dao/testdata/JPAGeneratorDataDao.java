ad0046b9b6741c311210a6ec2750cc3fd6b2668b
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-07 22:20:40 +0200

    Rewrite DAO class JPAGeneratorDataDao
    - now use Spring Data JPA
    - refactor interface GeneratorDataDao

--- SchoolManagementSpringDataJpaApi/src/main/java/ua/foxminded/hibernate/school/dao/testdata/JPAGeneratorDataDao.java
+++ SchoolManagementSpringDataJpaApi/src/main/java/ua/foxminded/hibernate/school/dao/testdata/JPAGeneratorDataDao.java
@@ -1,11 +1,12 @@
 package ua.foxminded.hibernate.school.dao.testdata;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
+import ua.foxminded.hibernate.school.dao.interfaces.CourseRepository;
 import ua.foxminded.hibernate.school.dao.interfaces.GeneratorDataDao;
+import ua.foxminded.hibernate.school.dao.interfaces.GroupRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentCourseRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentRepository;
 import ua.foxminded.hibernate.school.entity.Course;
 import ua.foxminded.hibernate.school.entity.Group;
 import ua.foxminded.hibernate.school.entity.Student;
@@ -14,49 +15,45 @@ import ua.foxminded.hibernate.school.entity.StudentCourseRelation;
 @Repository
 @Transactional
 public class JPAGeneratorDataDao implements GeneratorDataDao {
+	private final StudentRepository studentRepository;
+	private final GroupRepository groupRepository;
+	private final CourseRepository courseRepository;
+	private final StudentCourseRepository studentCourseRepository;
 
-  @PersistenceContext
-  private final EntityManager entityManager;
+	public JPAGeneratorDataDao(StudentRepository studentRepository, GroupRepository groupRepository,
+			CourseRepository courseRepository, StudentCourseRepository studentCourseRepository) {
+		this.studentRepository = studentRepository;
+		this.groupRepository = groupRepository;
+		this.courseRepository = courseRepository;
+		this.studentCourseRepository = studentCourseRepository;
+	}
 
-  public JPAGeneratorDataDao(EntityManager entityManager) {
-    this.entityManager = entityManager;
-  }
+	@Override
+	public void createStudent(Student student) {
+		studentRepository.save(student);
+	}
 
-  @Override
-  public void createStudent(Student student) {
-    entityManager.persist(student);
-  }
+	@Override
+	public void createGroup(Group group) {
+		groupRepository.save(group);
+	}
 
-  @Override
-  public void createGroup(Group group) {
-    entityManager.persist(group);
-  }
+	@Override
+	public void createCourse(Course course) {
+		courseRepository.save(course);
+	}
 
-  @Override
-  public void createCourse(Course course) {
-    entityManager.persist(course);
-  }
+	@Override
+	public void createCourseStudentRelation(StudentCourseRelation scRelation) {
+		studentCourseRepository.save(scRelation);
+	}
 
-  @Override
-  public void createCourseStudentRelation(StudentCourseRelation scRelation) {
-    entityManager.persist(scRelation);
-  }
-
-  @Override
-  public int rowsCount() {
-    String sql = """
-                  SELECT SUM (COUNT) FROM (
-        SELECT COUNT(*) AS COUNT FROM school.students
-        UNION ALL
-        SELECT COUNT(*) AS COUNT FROM school.groups
-        UNION ALL
-        SELECT COUNT(*) AS COUNT FROM school.course
-        UNION ALL
-        SELECT COUNT(*) AS COUNT FROM school.students_courses_checkouts
-        ) AS counts;
-                  """;
-    Query query = entityManager.createNativeQuery(sql);
-    return ((Number) query.getSingleResult()).intValue();
-  }
-
+	@Override
+	public int rowsCount() {
+		int studentCount = studentRepository.findAll().size();
+		int groupCount = groupRepository.findAll().size();
+		int courseCount = courseRepository.findAll().size();
+		int scrCount = studentCourseRepository.findAll().size();
+		return studentCount + groupCount + courseCount + scrCount;
+	}
 }
