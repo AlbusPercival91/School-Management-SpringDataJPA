486256fa0c852b7e159960c0e04a0a8bb6f13272
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-08 02:27:03 +0200

    start working with test classes

---
+++ SchoolManagementSpringDataJpaApi/src/test/java/ua/foxminded/hibernate/school/dao/JPACourseDaoTest.java
@@ -0,0 +1,111 @@
+package ua.foxminded.hibernate.school.dao;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import ua.foxminded.hibernate.school.dao.interfaces.CourseRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.GroupRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentCourseRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentRepository;
+import ua.foxminded.hibernate.school.dao.testdata.JPAGeneratorDataDao;
+import ua.foxminded.hibernate.school.entity.Course;
+import ua.foxminded.hibernate.school.testdata.CourseMaker;
+import ua.foxminded.hibernate.school.testdata.GroupMaker;
+import ua.foxminded.hibernate.school.testdata.StudentMaker;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+		JPACourseService.class }))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test-container")
+@Sql(scripts = { "/drop_data.sql", "/init_tables.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class JPACourseDaoTest {
+
+	@Autowired
+	private CourseRepository courseRepository;
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Autowired
+	private GroupRepository groupRepository;
+
+	@Autowired
+	private StudentCourseRepository studentCourseRepository;
+
+	private TestDataGenerator testData;
+
+	@BeforeEach
+	void setUp() {
+		testData = new TestDataGenerator(new StudentMaker(), new GroupMaker(), new CourseMaker(),
+				new JPAGeneratorDataDao(studentRepository, groupRepository, courseRepository, studentCourseRepository));
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "40", "100", "70" })
+	void testFindCoursesWithLessOrEqualsStudents_ShouldReturnCoursesWithLessOrEqualsStudents(int number) {
+		testData.createStudent();
+		testData.createCourse();
+		testData.createCourseStudentRelation();
+		List<Course> actual = courseRepository.findCoursesWithLessOrEqualsStudents(number);
+		Assertions.assertNotNull(actual);
+
+		Assertions.assertTrue(actual.size() > 0);
+	}
+
+	@Test
+	void testFindCoursesWithLessOrEqualsStudents_WhenStudentsZero_ShouldReturnEmptyList() {
+		testData.createStudent();
+		testData.createCourse();
+		testData.createCourseStudentRelation();
+		List<Course> actual = courseRepository.findCoursesWithLessOrEqualsStudents(0);
+
+		Assertions.assertTrue(actual.isEmpty());
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "History, TBD, Geography, TBD-2", "Art, TBD, Paint, TBD-3", "Sports, TBD, Yoga, TBD-5",
+			"English, TBD, Spanish, TBD-6", "123, TBD, 321, asdf", "%$#, TBD, $%^&, TBDTBD",
+			"!@-@$, )&-%^, Swimming, TBD" })
+	void testEditCourseNameAndDescription_ShouldReturnOneIfCourseUpdated(String courseName, String courseDescription,
+			String newCourseName, String newCourseDescription) {
+		Course course = new Course(courseName, courseDescription);
+		courseRepository.save(course);
+
+		Assertions.assertEquals(1, courseRepository.editCourseNameAndDescription(course.getCourseName(), newCourseName,
+				newCourseDescription));
+	}
+
+	@ParameterizedTest
+	@DisplayName("Should return 1 if 1 course deleted")
+	@CsvSource({ "History, TBD", "Swimming, TBD", "Paint, TBD-3", "Spanish, TBD-6", "Geography, TBD-2" })
+	void testDeleteCourseByName_ShouldReturnOneIfCourseDeleted(String courseName, String courseDescription) {
+		Course course = new Course(courseName, courseDescription);
+		courseRepository.save(course);
+
+		Assertions.assertEquals(1, courseRepository.deleteCourseByName(course.getCourseName()));
+	}
+
+	@Test
+	@DisplayName("Should return 10 when initiated course test data")
+	void testShowAllCourses_ShouldReturnAllCourses() {
+		testData.createCourse();
+		List<Course> actual = courseRepository.findAll();
+
+		Assertions.assertEquals(10, actual.size());
+	}
+
+}
---
+++ SchoolManagementSpringDataJpaApi/src/test/java/ua/foxminded/hibernate/school/dao/JPAStudentDaoTest.java
@@ -0,0 +1,141 @@
+package ua.foxminded.hibernate.school.dao;
+
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.*;
+import ua.foxminded.hibernate.school.dao.interfaces.CourseRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.GroupRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentCourseRepository;
+import ua.foxminded.hibernate.school.dao.interfaces.StudentRepository;
+import ua.foxminded.hibernate.school.dao.testdata.JPAGeneratorDataDao;
+import ua.foxminded.hibernate.school.entity.Course;
+import ua.foxminded.hibernate.school.entity.Student;
+import ua.foxminded.hibernate.school.entity.StudentCourseRelation;
+import ua.foxminded.hibernate.school.testdata.CourseMaker;
+import ua.foxminded.hibernate.school.testdata.GroupMaker;
+import ua.foxminded.hibernate.school.testdata.StudentMaker;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+		JPAStudentService.class }))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test-container")
+@Sql(scripts = { "/drop_data.sql", "/init_tables.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class JPAStudentDaoTest {
+
+	@Autowired
+	private CourseRepository courseRepository;
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Autowired
+	private GroupRepository groupRepository;
+
+	@Autowired
+	private StudentCourseRepository studentCourseRepository;
+
+	private TestDataGenerator testData;
+
+	@BeforeEach
+	void setUp() {
+		testData = new TestDataGenerator(new StudentMaker(), new GroupMaker(), new CourseMaker(),
+				new JPAGeneratorDataDao(studentRepository, groupRepository, courseRepository, studentCourseRepository));
+	}
+
+	@Test
+	void testFindStudentsRelatedToCourse_ShouldReturnNameOfCourses() {
+		testData.createCourse();
+		testData.createStudent();
+		testData.createCourseStudentRelation();
+
+		for (String s : new CourseMaker().generateCourses()) {
+			long studentCount = studentRepository.findStudentsRelatedToCourse(s).stream()
+					.filter(course -> course.toString().trim().contains(" ")).count();
+			Assertions.assertTrue(studentCount > 0);
+		}
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "12, Art", "190, Literature", "19, Computer Science", "21, Geography", "193, Physical Science",
+			"1, Life Science", "9, English", "2, Mathematics", "150, Sports", "7, History" })
+	void testAddStudentToTheCourse_ShouldReturnOneIfStudentAssigned(int studentId, String course) {
+		testData.createCourse();
+		testData.createStudent();
+
+		Assertions.assertEquals(1, studentRepository.addStudentToTheCourse(studentId, course));
+	}
+
+	@Test
+	void testAddNewStudent_ShouldReturnEqualsWhenNewStudentCreated() {
+		Student student = new Student(4, "Harry", "Potter");
+		studentRepository.save(student);
+		List<Student> actual = studentRepository.findAll();
+
+		Assertions.assertEquals(student.toString(), actual.toString());
+	}
+
+	@Test
+	void testDeleteStudentByID_ShouldReturnOneIfStudentRemoved() {
+		Student student = new Student(1, "Albus", "Dambldor");
+		studentRepository.save(student);
+		Assertions.assertEquals(1, studentRepository.findAll().size());
+
+		studentRepository.deleteById(student.getId());
+		Assertions.assertTrue(studentRepository.findAll().isEmpty());
+	}
+
+	void testGetStudentID_ShouldReturnAllStudentsID() {
+		testData.createStudent();
+		List<Integer> actual = studentRepository.getStudentID();
+
+		Assertions.assertEquals(200, actual.size());
+	}
+
+	@Test
+	void testRemoveStudentFromCourse_ShouldReturnOneIfStudentRemovedFromCourse() {
+		testData.createCourse();
+		testData.createStudent();
+		Student student = new Student(4, "Harry", "Potter");
+		Course course = new Course("Potions", "TBD");
+		StudentCourseRelation relation = new StudentCourseRelation(student.getId(), course.getId());
+		studentCourseRepository.save(relation);
+		int deleted = studentRepository.removeStudentFromCourse(student.getId(), course.getCourseName());
+
+		Assertions.assertEquals(1, deleted);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "4, Harry, Potter, 3, Ron, Wesley", "6, Draco, Malfoy, 7, Hermione, Granger" })
+	void testUpdateStudentById_ShouldReturnEqualsStringsWhenStudentUpdated(int groupId, String name, String lastName,
+			int newGroupId, String newName, String newLastName) {
+		Student student = new Student(groupId, name, lastName);
+		studentRepository.save(student);
+		Student updatedStudent = new Student(newGroupId, newName, newLastName);
+		studentRepository.updateStudentById(student.getId(), updatedStudent);
+		Optional<Student> actualStudent = studentRepository.findById(student.getId());
+		String actual = actualStudent.get().getId() + " " + actualStudent.get().getGroupId() + " "
+				+ actualStudent.get().getFirstName() + " " + actualStudent.get().getLastName();
+		String expected = student.getId() + " " + updatedStudent.getGroupId() + " " + updatedStudent.getFirstName()
+				+ " " + updatedStudent.getLastName();
+
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	void testShowAllStudents_ShouldReturnAllStudents() {
+		testData.createStudent();
+		List<Student> actual = studentRepository.findAll();
+
+		Assertions.assertEquals(200, actual.size());
+	}
+}
