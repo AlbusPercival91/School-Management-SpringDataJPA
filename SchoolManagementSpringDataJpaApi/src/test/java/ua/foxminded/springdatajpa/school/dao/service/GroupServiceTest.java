93bc48f222924d0f16941636ba2fa0aa9224b8a0
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-05-12 18:25:10 +0200

    edit GroupServiceTest:
    - rename testEditGroupName_ShouldReturnOneIfGroupUpdated to
    testEditGroupName_ShouldReturnNewGroupIfGroupUpdated
    
    - edit testEditGroupName_ShouldReturnNewGroupIfGroupUpdated

--- SchoolManagementSpringDataJpaApi/src/test/java/ua/foxminded/springdatajpa/school/dao/service/GroupServiceTest.java
+++ SchoolManagementSpringDataJpaApi/src/test/java/ua/foxminded/springdatajpa/school/dao/service/GroupServiceTest.java
@@ -77,11 +77,14 @@ class GroupServiceTest {
 	}
 
 	@ParameterizedTest
-	@CsvSource({ "aa-34, aa-35", "aa-35, 35-aa", "test, test-test", "123, 321", "aa-aa, bb-bb", "00-00, 11-11",
-			"!@-@$, )&-%^" })
-	void testEditGroupName_ShouldReturnOneIfGroupUpdated(Group group, Group newGroup) {
-		groupRepository.save(group);
-		Assertions.assertEquals(1, groupService.editGroupName(group.getGroupName(), newGroup.getGroupName()));
+	@CsvSource({ "1,aa-34, aa-35", "1,aa-35, 35-aa", "1, test, test-test", "1, 123, 321", "1, aa-aa, bb-bb",
+			"1, 00-00, 11-11", "1, !@-@$, )&-%^" })
+	void testEditGroupName_ShouldReturnOneIfGroupUpdated(int id, Group group, Group newGroup) {
+		Group expectedGroup = new Group(newGroup.getGroupName());
+		expectedGroup.setId(id);
+		groupService.createGroup(group);
+		Group actualGroup = groupService.editGroupName(group.getGroupName(), newGroup.getGroupName());
+		Assertions.assertEquals(expectedGroup.toString(), actualGroup.toString());
 	}
 
 	@ParameterizedTest
